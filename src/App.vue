acc5c0a4495103d6d6acd84586333ebe1cf598a0
zhangbaiming <973813763@qq.com>
2020-10-23 14:35:34 +0800

    项目初始化-调整初始化目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div>
+  <!-- 路由出口 -->
     <router-view/>
+    <h1>头条11</h1>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App'
 }
+</script>
 
-#nav {
-  padding: 30px;
+<style lang="scss" scoped>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
