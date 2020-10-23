d01e0dd257ffa94bf25d52f4ed4572ab6b278786
zhangbaiming <973813763@qq.com>
2020-10-23 16:06:04 +0800

    项目初始化，引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载Vant 全局样式
+import Vant from 'vant'
+// 加载全局的样式
+import 'vant/lib/index.css'
+
 // 加载全局样式
 import './styles/index.scss'
+
+// 注册使用Vant 组件库
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
